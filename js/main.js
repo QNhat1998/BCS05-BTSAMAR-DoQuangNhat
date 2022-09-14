46bbdb8cd515ac895204c88159ab873b818a5de2
QNhat1998 <46757841+QNhat1998@users.noreply.github.com>
2022-09-14 14:41:55 +0700

    complete

--- js/main.js
+++ js/main.js
@@ -12,3 +12,27 @@ btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '300');
 });
+
+var testimonialThumbs = new Swiper(".testimonial-thumbs", {
+    spaceBetween: 10,
+    slidesPerView: 3,
+    centeredSlides: true,
+    freeMode: true,
+    autoplay: true,
+    loop: true,
+    speed: 3000
+  });
+  var testimonialContent = new Swiper(".testimonial-comment", {
+    spaceBetween: 10,
+    autoplay: true,
+    loop: true,
+    speed: 3000,
+    thumbs: {
+      swiper: testimonialThumbs
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    }
+  });
+  
