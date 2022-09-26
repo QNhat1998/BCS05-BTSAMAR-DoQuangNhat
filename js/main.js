9654af338715cadb7137fe302818dcad8e111b85
QNhat1998 <46757841+QNhat1998@users.noreply.github.com>
2022-09-26 13:39:22 +0700

    demo

--- js/main.js
+++ js/main.js
@@ -35,4 +35,22 @@ var testimonialThumbs = new Swiper(".testimonial-thumbs", {
       clickable: true
     }
   });
+
+  $(".count").each(function () {
+    $(this)
+      .prop("Counter", 0)
+      .animate(
+        {
+          Counter: $(this).text(),
+        },
+        {
+          duration: 4000,
+          easing: "swing",
+          step: function (now) {
+            now = Number(Math.ceil(now)).toLocaleString('en');
+                                  $(this).text(now);
+          },
+        }
+      );
+  });
   
